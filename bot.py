dc821b25fc348cdf277be3bb2fc90d39162c99a7
Xernosz <72986510+Xernosz@users.noreply.github.com>
2021-02-02 10:12:35 -0800

    Part 6: Magic 8Ball command!

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # Importing Shit
 import discord
 from discord.ext import commands, tasks
+import random
 # Importing Shit
 
 # Setup
@@ -16,7 +17,7 @@ async def on_ready():
 	print(f"Connected to Bot: {client.user.name}")
 	print(f"Bot ID: {client.user.id}")
 	print(f"Currently in: {len(client.guilds)} server(s)!")
-	print(f"The current latency of the bot is {round(client.latency * 1000)} ms")
+	print(f"Latency = {round(client.latency * 1000)} ms")
 ## Making sure the bot is online
 
 # Events
@@ -26,8 +27,38 @@ async def on_ready():
 ## Ping
 @client.command(aliases=["latency"])
 async def ping(ctx):
-	await ctx.send(f"Pong! The latency is: {round(client.latency * 1000)} miliseconds!")
+	await ctx.send(f"The current latency is: {round(client.latency * 1000)} miliseconds")
 ## Ping
+
+## 8Ball
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = [' It is certain.',
+                 'It is decidedly so.',
+                 'Without a doubt.',
+                 'Yes – definitely.',
+                 'You may rely on it.',
+                 'As I see it, yes.',
+                 'Most likely.',
+                 'Outlook good.',
+                 'Yes.',
+                 'Signs point to yes.',
+                 'Reply hazy, try again.',
+                 'Ask again later.',
+                 'Better not tell you now.',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 'Don\'t count on it.',
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Very doubtful.', ]
+    if question:
+        await ctx.send(f'Question: {question} \nAnswer: {random.choice(responses)}')
+    else:
+        await ctx.send("You must ask a question for the magic 8-ball to answer!")
+
+## 8Ball
 # Commands
 
 client.run('YOUR_TOKEN')
