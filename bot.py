2464dda25ed41e47609f059ebb12d189e8a96901
Xernosz <72986510+Xernosz@users.noreply.github.com>
2021-02-02 02:35:20 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,28 @@
+# Importing Shit
 import discord
 from discord.ext import commands, tasks
+# Importing Shit
 
+# Setup
 client = commands.Bot(command_prefix='%')
+# Setup
 
+# Events
+## Making sure the bot is online
 @client.event
 async def on_ready():
 	print("Bot is ready!")
+## Making sure the bot is online
 
+# Events
 
+# Commands
+
+## Ping
 @client.command()
 async def ping(ctx):
 	await ctx.send('Pong!')
+## Ping
+# Commands
 
 client.run('ODA2MDk3MTM3MjM3ODg0OTU4.YBkedg.z2TIFvq101YXGOhVewnVn1Lec9Y')
